73f721255f69afcfe5f2577fc2a3a5fa739ef52a
Daniel Iglesias <danieliglesias@dbe4efa67.dslam-172-26-25-237-282-306-pod-03.dsl.cantv.net>
2017-06-28 01:32:12 -0400

    passing arribo on url to phpexcel

--- Views/Pages/excel.php
+++ Views/Pages/excel.php
@@ -61,14 +61,14 @@ $objPHPExcel->getActiveSheet()->getColumnDimension('AA')->setAutoSize(true);
 $objPHPExcel->getActiveSheet()->getColumnDimension('AC')->setAutoSize(true);
 
 $objPHPexcel2 = PHPExcel_IOFactory::load('Formato_reporte_SAO.xlsx');
-$objClonedWorksheet = clone $objPHPexcel2->getSheetByName('Worksheet');
-//$objPHPExcel->addExternalSheet($objClonedWorksheet);
+$objClonedWorksheet = clone $objPHPexcel2->getSheetByName('Hoja1');
+// $objPHPExcel->addExternalSheet($objClonedWorksheet);
 // Add some data
-$sheetIndex = $objPHPExcel->getIndex($objPHPExcel-> getSheetByName('Worksheet'));
-$objPHPExcel->removeSheetByIndex($sheetIndex);
-// $sheetIndex = $objPHPExcel->getIndex($objPHPExcel-> getSheetByName('Hoja1'));
+ //$sheetIndex = $objPHPExcel->getIndex($objPHPExcel-> getSheetByName('Worksheet'));
+// $objPHPExcel->removeSheetByIndex($sheetIndex);
+$sheetIndex = $objPHPExcel->getIndex($objPHPExcel-> getSheetByName('Hoja1'));
 $objPHPExcel->setActiveSheetIndex($sheetIndex)
-              ->setCellValue('L1', $id_arribo);
+              ->setCellValue('L1', 'proceso');
 // $objPHPExcel->setActiveSheetIndex($sheetIndex)
 //               ->setCellValue('L13', $var[0]['subproceso']);
 // $objPHPExcel->setActiveSheetIndex($sheetIndex)
