a426ee756a3ee8ac8b1a0a6a5372167fba509328
Daniel Iglesias <iglesias.dan@gmail.com>
2017-06-28 15:50:25 -0400

    report

--- Views/Pages/table_estudios.php
+++ Views/Pages/table_estudios.php
@@ -51,7 +51,7 @@
       <td>{{post.Fecha_arribo | date:'dd-MM-yyyy '}}</td>
       <td>
         <a ui-sref="detalle_estudio({id_estudio: post.ID_arribo})"><i class="fa fa-pencil" data-toggle="tooltip" data-placement="bottom" title="Editar Puerto"></i></a>
-          <a href="./excel.php"><i class="fa fa-download" data-toggle="tooltip" data-placement="bottom" title="Descargar PDF"></i></a>
+          <a href="http://107.180.66.98:90//SAO/Views/Pages/excel.php"><i class="fa fa-download" data-toggle="tooltip" data-placement="bottom" title="Descargar PDF"></i></a>
         <a href=""><i class="fa fa-trash eliminar" data-toggle="tooltip" data-placement="bottom" title="Eliminar Puerto"></i></a>
       </td>
     </tr>
