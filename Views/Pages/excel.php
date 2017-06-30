7d651b4fda521af7444d2a448ec164de358dd310
Daniel Iglesias <iglesias.dan@gmail.com>
2017-06-30 15:07:46 -0400

    excel

--- Views/Pages/excel.php
+++ Views/Pages/excel.php
@@ -27,7 +27,7 @@ require_once('conexion.php');
   if ($row2=mysqli_fetch_array($resultado2)) {
      // var_dump($row2);
   }
-  $consulta3="SELECT * FROM estudio WHERE ID_arribo=1";
+  $consulta3="SELECT * FROM estudio WHERE ID_arribo=1 ORDER BY ID_estudio";
   $resultado3 = $conexion->query($consulta3);
   if ($row3=mysqli_fetch_array($resultado3)) {
       echo $consulta3;
