0b20311a918591dfa01ff7ba16cf523ec7420c2f
Daniel Iglesias <iglesias.dan@gmail.com>
2017-07-01 23:50:41 -0400

    bug date

--- App/Controllers/buqueController.js
+++ App/Controllers/buqueController.js
@@ -236,7 +236,7 @@ $http({
     // var str = json["Fecha_arribo"];
     // var res = str.split("T");
     // json["Fecha_arribo"] = res[0];
-
+    console.log(json);
 
     if ($stateParams.id_estudio === undefined) {
       // console.log(this.posts1.Fecha)
