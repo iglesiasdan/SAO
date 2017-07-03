ec44a380d02ae1e33112ad8bbaaedefb3da4299f
Daniel Iglesias <iglesias.dan@gmail.com>
2017-07-03 19:39:15 -0400

    validation

--- Views/Pages/agregar_arribo.php
+++ Views/Pages/agregar_arribo.php
@@ -2,6 +2,7 @@
             <header class="page-header">
                <h2>Formulario de Verificacion de Agua de Lastre</h2>
             </header>
+            <ng-form id="Form" name="Form" >
             <div >
             <div class="col-md-12" id="buque">
                <section class="panel panel-featured panel-featured-primary">
@@ -11,7 +12,7 @@
                   <div class="panel-body">
                      <div class="form-group">
                         <div class="col-md-6">
-                           <div class="col-md-12">
+                           <div ng-class="{'has-error': Form.$invalid}" class="col-md-12">
                               <label class="control-label" style="text-align: left;"><strong>Buque:</strong></label>
                               <select id="buque" name="buque" class="form-control mb-md" required ng-value="prueba"  ng-model="selectedBuque" ng-options="post.Nombre_buque for post in posts3 track by post.ID_buque">
                                  <option value="" disabled selected>Seleccionar Buque</option>
@@ -19,8 +20,9 @@
                                  <option value='2'>Buque 2</option>
                                  <option value='3'>Buque 3</option>
                               </select>
+                               <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
                            </div>
-                           <div class="col-md-12">
+                           <div ng-class="{'has-error': Form.$invalid}" class="col-md-12">
                               <label class="control-label" style="text-align: left;"><strong>Puerto:</strong></label>
                               <select id="puerto" name="puerto" class="form-control mb-md" required ng-model="selectedPuerto" ng-options="post.Nombre_puerto for post in posts4 track by post.ID_puerto">
                                  <option value="" disabled selected>Seleccionar Puerto</option>
@@ -28,8 +30,9 @@
                                  <option value='2'>Puerto 2</option>
                                  <option value='3'>Puerto 3</option>
                               </select>
+                               <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
                            </div>
-                           <div class="col-md-12">
+                           <div ng-class="{'has-error': Form.$invalid}" class="col-md-12">
                               <label class="control-label" style="text-align: left;"><strong>Agencia:</strong></label>
                               <select id="agencia" name="agencia" class="form-control mb-md" required ng-model="selectedAgencia" ng-options="post.Nombre_agencia for post in posts5 track by post.ID_agencia">
                                  <option value="" disabled selected>Seleccionar Agencia</option>
@@ -37,6 +40,7 @@
                                  <option value='2'>Puerto 2</option>
                                  <option value='3'>Puerto 3</option>
                               </select>
+                               <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
                            </div>
                         </div>
                         <div class="col-md-6">
@@ -47,28 +51,26 @@
 
                      </div>
                      </br>
-                    <ng-form id="test" name="test" >
-      <div ng-class="{'has-error': test.$invalid}" class="form-group" >
-         <input id="field" name="field" required class="form-control" ng-model="field" type="text"/>
-         <div class="help-block error" ng-show="test.field.$error.required">Required</div>
-      </div>
-    </ng-form>
                      <div class="form-group">
-                        <div class="col-md-6">
+                        <div ng-class="{'has-error': Form.$invalid}" class="col-md-6">
                            <label class="control-label" style="text-align: left;"><strong>Fecha de Arribo:</strong></label>
-                           <input type="date" class="form-control" name="fecha_arribo" id="fecha_arribo" ng-model="posts1.Fecha">
+                           <input type="date" class="form-control" name="fecha_arribo" id="fecha_arribo" ng-model="posts1.Fecha" required>
+                            <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
                         </div>
-                        <div class="col-md-6">
+                        <div ng-class="{'has-error': Form.$invalid}" class="col-md-6">
                            <label class="control-label" style="text-align: left;"><strong>Calado de Proa:</strong></label>
-                           <input type="number" class="form-control" name="calado_proa" id="calado_proa" placeholder="Calado de Proa" ng-model="posts1.Calado_proa" ng-required="true">
+                           <input type="number" class="form-control" name="calado_proa" id="calado_proa" placeholder="Calado de Proa" ng-model="posts1.Calado_proa" required>
+                            <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
                         </div>
-                        <div class="col-md-6">
+                        <div ng-class="{'has-error': Form.$invalid}" class="col-md-6">
                            <label class="control-label" style="text-align: left;"><strong>Calado de Popa:</strong></label>
-                           <input type="text" class="form-control" name="calado_popa" id="calado_popa" placeholder="Calado de Popa" ng-model="posts1.Calado_popa">
+                           <input type="text" class="form-control" name="calado_popa" id="calado_popa" placeholder="Calado de Popa" ng-model="posts1.Calado_popa" required>
+                            <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
                         </div>
-                        <div class="col-md-6">
+                        <div ng-class="{'has-error': Form.$invalid}" class="col-md-6">
                            <label class="control-label" style="text-align: left;"><strong>Diferencia de Calado:</strong></label>
-                           <input type="text" class="form-control" name="diferencia_calado" id="diferencia_calado" placeholder="Diferencia de Calado" ng-model="posts1.Diferencias_calado">
+                           <input type="text" class="form-control" name="diferencia_calado" id="diferencia_calado" placeholder="Diferencia de Calado" ng-model="posts1.Diferencias_calado" required>
+                            <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
                         </div>
                   </div>
                   </div>
@@ -94,3 +96,9 @@
                                        </section>
                                     </div>
                   </div>
+</ng-form>
+<div ng-class="{'has-error': Form.$invalid}" class="form-group" >
+         <input id="field" name="field" required class="form-control" ng-model="field" type="text"/>
+         <div class="help-block error" ng-show="Form.field.$error.required">Campo Requerido.!</div>
+      </div>
+    
